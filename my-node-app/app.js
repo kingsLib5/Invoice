92804477353f8da4fb4106602e529f799feb9559
kingsLib5 <libertykings88@gmail.com>
2024-07-08 20:29:30 +0100

    new codes

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
 const express = require('express');
 const app = express();
 const port = 3000;
